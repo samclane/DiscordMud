e882582ab90c220b50425221816aacf85379eb51
Sawyer McLane <sawyer@dedesignworks.com>
2017-12-08 15:10:41 -0600

    Added some persistance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import shelve
 import os
 
 client = discord.Client()
@@ -7,6 +8,18 @@ client = discord.Client()
 token = os.environ.get("DISCORD_BOT_TOKEN")
 
 prefix = '*'
+
+settings_filename = os.path.join(os.path.dirname(__file__), '/settings')
+
+users_filename = os.path.join(os.path.dirname(__file__), '/users')
+
+world_filename = os.path.join(os.path.dirname(__file__), '/world')
+
+settings = shelve.open(settings_filename)
+
+users = shelve.open(users_filename)
+
+world = shelve.open(world_filename)
 
 @client.event
 async def on_ready():
@@ -17,8 +30,18 @@ async def on_ready():
 
 
 @client.event
-async def on_message(message):
-    if message.content.startswith(prefix + 'ping'):
-        await client.send_message(message.channel, 'Pong!')
+async def on_message(message: discord.Message):
+    if message.content.startswith(prefix):
+        text = message.content.replace('prefix', '') #strip the prefix from the message
+        user = message.author
+
+
+@client.event
+async def on_server_remove(server: discord.Server):
+    print('Logging out')
+    print('------')
+    settings.close()
+    users.close()
+    world.close()
 
 client.run(token)
