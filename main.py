5046184a3202f601e2900e7068ae3e224599df10
Sawyer McLane <samclane@gmail.com>
2017-12-09 16:04:31 -0600

    Shelve saves new users.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import discord
-import asyncio
-import shelve
+import atexit
 import os
+import shelve
+
+import discord
 
 client = discord.Client()
 
@@ -9,17 +10,14 @@ token = os.environ.get("DISCORD_BOT_TOKEN")
 
 prefix = '*'
 
-settings_filename = os.path.join(os.path.dirname(__file__), '/settings')
-
-users_filename = os.path.join(os.path.dirname(__file__), '/users')
-
-world_filename = os.path.join(os.path.dirname(__file__), '/world')
+settings_filename = "settings.dat"
+users_filename = "users.dat"
+world_filename = "world.dat"
 
 settings = shelve.open(settings_filename)
-
 users = shelve.open(users_filename)
-
 world = shelve.open(world_filename)
+
 
 @client.event
 async def on_ready():
@@ -32,16 +30,38 @@ async def on_ready():
 @client.event
 async def on_message(message: discord.Message):
     if message.content.startswith(prefix):
-        text = message.content.replace('prefix', '') #strip the prefix from the message
+        text = message.content.replace(prefix, '')  # strip the prefix from the message
         user = message.author
+        channel = message.channel
+        if not (user.id in users.keys()):
+            users[user.id] = MUDUser(user)
+            await register_user(users[user.id], channel)
 
 
 @client.event
 async def on_server_remove(server: discord.Server):
     print('Logging out')
     print('------')
+    save_files()
+
+
+@atexit.register
+def save_files():
     settings.close()
     users.close()
     world.close()
+
+
+class MUDUser:
+    DiscordUser = None
+
+    def __init__(self, discord_user: discord.User):
+        self.DiscordUser = discord_user
+
+
+async def register_user(mud_user: MUDUser, channel: discord.Channel):
+    await client.send_message(channel, mud_user.DiscordUser.name + ", would you like to join the MUD?")
+    return
+
 
 client.run(token)
