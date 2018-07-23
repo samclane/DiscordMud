e587467336bb27413efacc9455149c3f3493aedf
Sawyer McLane <samclane@gmail.com>
2018-07-23 14:38:40 -0600

    Store sprites on init (instead of each paint). Zoom and pan are now using builtin QGraphicsView functions.

--- gui/ui.py
+++ gui/ui.py
@@ -1,7 +1,7 @@
 """ Future home of the PyQt5 GUI """
 from PyQt5.QtCore import pyqtSignal, Qt, QRectF
-from PyQt5.QtGui import QIcon, QImage
-from PyQt5.QtWidgets import QMainWindow, QAction, QStyle, QGraphicsView, QGraphicsScene, QGraphicsObject
+from PyQt5.QtGui import QIcon, QImage, QBrush, QColor
+from PyQt5.QtWidgets import QMainWindow, QAction, QStyle, QGraphicsView, QGraphicsScene, QGraphicsObject, QFrame
 
 from gamelogic.gamespace import Town, Wilds
 
@@ -62,48 +62,80 @@ class MainWindow(QMainWindow):
         super().update()
         self.worldFrame.update()
 
+    def resizeEvent(self, event):
+        self.worldFrame.fitInView()
+
 
 class WorldFrame(QGraphicsView):
     msg2Statusbar = pyqtSignal(str)
 
     def __init__(self, parent, world):
         super().__init__(parent)
-        self.pointerMode = PointerMode.Normal
-        scene = QGraphicsScene(self)
-        self.world = world
-        self.view = WorldView(self, world)
-        scene.addItem(self.view)
-        self.setScene(scene)
-        self.setCacheMode(QGraphicsView.CacheBackground)
+        self._zoom = 0
+        self._scene = QGraphicsScene(self)
+        self._world = world
+        self._worldview = WorldView(self, world)
+        self._scene.addItem(self._worldview)
+        self.setScene(self._scene)
         self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
         self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setBackgroundBrush(QBrush(QColor(30, 30, 30)))
+        self.setFrameShape(QFrame.NoFrame)
+        self.pointerMode = PointerMode.Normal
+        self.setCacheMode(QGraphicsView.CacheBackground)
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
 
-        self.setFocusPolicy(Qt.StrongFocus)
-        self.setMouseTracking(True)
+    def fitInView(self, scale=True):
+        rect = QRectF(self._worldview.boundingRect())
+        if not rect.isNull():
+            self.setSceneRect(rect)
+            unity = self.transform().mapRect(QRectF(0, 0, 1, 1))
+            self.scale(1 / unity.width(), 1 / unity.height())
+            viewrect = self.viewport().rect()
+            scenerect = self.transform().mapRect(rect)
+            factor = min(viewrect.width() / scenerect.width(),
+                         viewrect.height() / scenerect.height())
+            self.scale(factor, factor)
+            self._zoom = 0
 
     def resetViewport(self):
-        self.view.resetViewport()
+        self.fitInView()
+        self.update()
 
     def mouseMoveEvent(self, event):
         point = self.mapToScene(event.x(), event.y())
         ex, ey = point.x(), point.y()
-        gridx, gridy = self.view.pixToGrid(ex, ey)
+        gridx, gridy = self._worldview.pixToGrid(ex, ey)
         self.currentGridPoint = (gridx, gridy)
         landmark = " "
-        if 0 < gridx < self.world.Width and 0 < gridy < self.world.Height:
-            space = self.world.Map[gridy][gridx]
-            if space in self.world.Towns or space in self.world.Wilds:
+        if 0 < gridx < self._world.Width and 0 < gridy < self._world.Height:
+            space = self._world.Map[gridy][gridx]
+            if space in self._world.Towns or space in self._world.Wilds:
                 landmark += space.Name
         self.msg2Statusbar.emit("({}, {}) {}".format(int(gridx), int(gridy), landmark))
         self.update()
+        super().mouseMoveEvent(event)
+
+    def wheelEvent(self, event):
+        if event.angleDelta().y() > 0:
+            factor = 1.25
+            self._zoom += 1
+        else:
+            factor = 0.8
+            self._zoom -= 1
+        if self._zoom > 0:
+            self.scale(factor, factor)
+        elif self._zoom == 0:
+            self.fitInView()
+        else:
+            self._zoom = 0
 
     def update(self):
         super().update()
-        self.view.update()
+        self._worldview.update()
 
     def addTownMode(self, event):
         self.pointerMode = PointerMode.AddTown
-
 
 class WorldView(QGraphicsObject):
     msg2Statusbar = pyqtSignal(str)
@@ -112,68 +144,47 @@ class WorldView(QGraphicsObject):
         super().__init__()
         self.parent = parent
         self.world = world
+        self.dirtpix = QImage(r"res/sprites/grass.png")
+        self.townpix = QImage(r"res/sprites/town.png")
+        self.wildpix = QImage(r"res/sprites/wild.png")
+        self.playerpix = QImage(r"res/sprites/player.png")
         self.resetViewport()
 
     def boundingRect(self):
         return QRectF(0, 0, 1000, 1000)
 
     def resetViewport(self):
-        self.scale = 1
-        self.panX = 0
-        self.panY = 0
         self.update()
 
     def gridToPix(self, x, y):
         rect = self.boundingRect()
         canvasTop = rect.bottom() - self.world.Height * self.squareHeight()
-        xcoord, ycoord = rect.left() + self.panX + x * self.squareWidth(), \
-                         canvasTop + self.panY + y * self.squareHeight()
+        xcoord, ycoord = rect.left() + x * self.squareWidth(), \
+                         canvasTop + y * self.squareHeight()
         return xcoord, ycoord
 
     def pixToGrid(self, x, y):
         rect = self.boundingRect()
         canvasTop = rect.bottom() - self.world.Height * self.squareHeight()
-        gridx, gridy = (x - rect.left() - self.panX) // self.squareWidth(), \
-                       (y - canvasTop - self.panY) // self.squareHeight()
+        gridx, gridy = (x - rect.left()) // self.squareWidth(), \
+                       (y - canvasTop) // self.squareHeight()
         return int(gridx), int(gridy)
 
     def squareWidth(self):
         '''returns the width of one square'''
 
-        return int((self.boundingRect().width() / self.world.Width) * self.scale)
+        return int((self.boundingRect().width() / self.world.Width))
 
     def squareHeight(self):
         '''returns the height of one square'''
 
-        return int((self.boundingRect().height() / self.world.Height) * self.scale)
-
-    def wheelEvent(self, event):
-        self.scale += event.delta() / 2880
-        self.update()
-
-    def keyPressEvent(self, event):
-        key = event.key()
-        xresolution = self.squareWidth()
-        yresolution = self.squareHeight()
-
-        if key == Qt.Key_Left:
-            self.panX += xresolution
-        elif key == Qt.Key_Right:
-            self.panX -= xresolution
-        elif key == Qt.Key_Down:
-            self.panY -= yresolution
-        elif key == Qt.Key_Up:
-            self.panY += yresolution
-        else:
-            super().keyPressEvent(event)
-
-        self.update()
+        return int((self.boundingRect().height() / self.world.Height))
 
     def paint(self, painter, option, widget):
-        dirtpix = QImage(r"res/sprites/grass.png").scaledToWidth(self.squareWidth()).scaledToHeight(self.squareHeight())
-        townpix = QImage(r"res/sprites/town.png").scaledToWidth(self.squareWidth()).scaledToHeight(self.squareHeight())
-        wildpix = QImage(r"res/sprites/wild.png").scaledToWidth(self.squareWidth()).scaledToHeight(self.squareHeight())
-        playerpix = QImage(r"res/sprites/player.png").scaledToWidth(self.squareWidth()).scaledToHeight(
+        dirtpix = self.dirtpix.scaledToWidth(self.squareWidth()).scaledToHeight(self.squareHeight())
+        townpix = self.townpix.scaledToWidth(self.squareWidth()).scaledToHeight(self.squareHeight())
+        wildpix = self.wildpix.scaledToWidth(self.squareWidth()).scaledToHeight(self.squareHeight())
+        playerpix = self.playerpix.scaledToWidth(self.squareWidth()).scaledToHeight(
             self.squareHeight())
 
         # Draw terrain
