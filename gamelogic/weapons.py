cf4c96b2054416ea8649df8f8dad8b8732c151e9
Sawyer McLane <samclane@gmail.com>
2018-08-08 18:32:37 -0600

    Added more weapons attributes & properties, along with corresponding tests.

--- gamelogic/weapons.py
+++ gamelogic/weapons.py
@@ -4,13 +4,15 @@ from gamelogic.items import Equipment
 # Remember: def func(a, b, kw1=None, *args, **kwargs):
 
 
-class AmmoType:
+class ProjectileType:
     Thrown = 0
     Bullet = 1
+    Rocket = 2
 
 
 class Caliber:
     BB = 0
+    MM_9 = 1
 
 
 class Weapon(Equipment):
@@ -18,28 +20,68 @@ class Weapon(Equipment):
 
     def __init__(self, base_damage, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if base_damage < 0:
+            raise ValueError("base_damage must be 0 or greater.")
         self.BaseDamage = base_damage
 
 
 class RangedWeapon(Weapon):
-    def __init__(self, *args, **kwargs):
+    Range: int
+
+    def __init__(self, range_=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if range_ < 1:
+            raise ValueError("Range must be 1 or greater.")
+        self.Range = range_
 
 
 class ProjectileWeapon(RangedWeapon):
     AmmoType: int
+    Capacity: int
 
-    def __init__(self, ammo_type, *args, **kwargs):
+    def __init__(self, projectile_type, capacity=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.AmmoType = ammo_type
+        self.AmmoType = projectile_type
+        if capacity < 1:
+            raise ValueError("Capacity must be 1 or greater.")
+        self.Capacity = capacity
+        self._currentCapacity = capacity
+
+    @property
+    def isSingleShot(self) -> bool:
+        return self.Capacity == 1
+
+    @property
+    def currentCapacity(self) -> int:
+        return self._currentCapacity
+
+    @property
+    def isEmpty(self) -> bool:
+        return self.currentCapacity == 0
+
+    def fire(self):
+        self._currentCapacity -= 1
+
+    def reload(self):
+        self._currentCapacity = self.Capacity
 
 
 class Firearm(ProjectileWeapon):
     Caliber: int
 
-    def __init__(self, caliber, *args, **kwargs):
-        super().__init__(AmmoType.Bullet, *args, **kwargs)
+    def __init__(self, caliber, burst_size=1, *args, **kwargs):
+        super().__init__(ProjectileType.Bullet, *args, **kwargs)
         self.Caliber = caliber
+        self.BurstSize = burst_size
+
+    def fire(self):
+        self._currentCapacity -= self.BurstSize
+
+
+class Pistol(Firearm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class MeleeWeapon(Weapon):
--- test.py
+++ test.py
@@ -45,7 +45,10 @@ class PlayerTest(unittest.TestCase):
         i = Wilds(2, 2, 'Hidden Forrest')
         e = EncounterEvent(.5, "You find a penny in the road.",
                            {"Pick it up": "Gain a penny", "Leave it": "Gain nothing"})
+        e2 = EncounterEvent(.5, "You find a scav in the road.",
+                            {"Shoot him": "Gain a gun", "Leave him": "Get shot (-5hp)"})
         i.addEvent(e)
+        i.addEvent(e2)
         w.addWilds(i)
         player = PlayerCharacter(None, None)
         player.Name = "Algor"
@@ -64,12 +67,29 @@ class WeaponsTest(unittest.TestCase):
         weight = 100
         value = 100
         dmg = 5
+        rng = 2
         chance = 0.5
         factor = 0.1
+        capacity = 30
         w = Weapon(dmg, name=name, weight=weight, base_value=value)
-        r = RangedWeapon(dmg, name=name, weight=weight, base_value=value)
-        p = ProjectileWeapon(AmmoType.Thrown, dmg, name=name, weight=weight, base_value=value)
-        f = Firearm(Caliber.BB, dmg, name=name, weight=weight, base_value=value)
+        r = RangedWeapon(rng, base_damage=dmg, name=name, weight=weight, base_value=value)
+        p = ProjectileWeapon(ProjectileType.Thrown, base_damage=dmg, name=name, weight=weight, base_value=value)
+        f1 = Firearm(Caliber.BB, burst_size=3, capacity=capacity, base_damage=dmg, name=name, weight=weight,
+                     base_value=value)
+        f2 = Firearm(Caliber.BB, capacity=1, base_damage=dmg, name=name, weight=weight, base_value=value)
         m = MeleeWeapon(dmg, name=name, weight=weight, base_value=value)
         b = BladedWeapon(chance, factor, dmg, name=name, weight=weight, base_value=value)
         bu = BluntWeapon(chance, dmg, name=name, weight=weight, base_value=value)
+
+        self.assertEqual(r.Range, rng)
+        self.assertEqual(f1.Range, 1)
+        self.assertEqual(f1.Capacity, capacity)
+        self.assertEqual(f1.currentCapacity, capacity)
+        self.assertFalse(f1.isSingleShot)
+        self.assertTrue(f2.isSingleShot)
+        with self.assertRaises(AttributeError) as _:
+            f1.currentCapacity = 5
+        f1.fire()
+        self.assertEqual(f1.currentCapacity, capacity - f1.BurstSize)
+        f2.fire()
+        self.assertTrue(f2.isEmpty)
